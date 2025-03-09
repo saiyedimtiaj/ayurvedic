b7ae5a0854e25887d0bfe26ff030ea3a5765b4e2
saiyedimtiaj <hridoyemtiaz0@gmail.com>
2025-03-09 22:23:45 +0600

    test facebook pixel

--- src/hooks/useFacebookPixel.tsx
+++ src/hooks/useFacebookPixel.tsx
@@ -1,31 +1,16 @@
 "use client";
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect } from "react";
+import ReactPixel from "react-facebook-pixel";
+
+const options = {
+  autoConfig: true,
+  debug: false,
+};
 
 const useFacebookPixel = () => {
   useEffect(() => {
-    if (typeof window === "undefined") return; // Prevents running on SSR
-
-    if (!window.fbq) {
-      window.fbq = function (...args: any[]) {
-        if (window.fbq.callMethod) {
-          window.fbq.callMethod(...args);
-        } else {
-          (window.fbq.queue = window.fbq.queue || []).push(args);
-        }
-      };
-      window.fbq.loaded = true;
-      window.fbq.version = "2.0";
-      window.fbq.queue = [];
-
-      const script = document.createElement("script");
-      script.async = true;
-      script.src = "https://connect.facebook.net/en_US/fbevents.js";
-      document.head.appendChild(script);
-    }
-
-    window.fbq("init", "1170484508077907"); // Replace with your Pixel ID
-    window.fbq("track", "PageView");
+    ReactPixel.init("1170484508077907", undefined, options);
+    ReactPixel.pageView();
   }, []);
 };
 
